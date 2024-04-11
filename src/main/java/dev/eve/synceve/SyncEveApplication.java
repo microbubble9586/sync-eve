d219339a258b394aefd7214f7b0e017510f36cf5
memoryFade <archprotocal@gmail.com>
2024-04-11 22:11:41 +0800

    init

---
+++ src/main/java/dev/eve/synceve/SyncEveApplication.java
@@ -0,0 +1,13 @@
+package dev.eve.synceve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncEveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncEveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/eve/synceve/SyncEveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eve.synceve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncEveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
